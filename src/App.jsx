3b6d9614dd1debf74aa5d909142bfa1cbc2bd265
raselworshop <raselworshop@gmail.com>
2024-10-21 22:14:20 -0400

    added the neccessary files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Blogs from './Component/Blogs/Blogs'
+import Header from './Component/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
